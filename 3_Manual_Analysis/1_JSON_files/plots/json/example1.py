d521d15bdc74f6af0e4249ac1eff7ddf263b84cf
David Schledewitz <62600861+DavidSchledewitz@users.noreply.github.com>
2021-06-25 10:15:16 +0200

    .

--- 3_Manual_Analysis/1_JSON_files/plots/json/example1.py
+++ 3_Manual_Analysis/1_JSON_files/plots/json/example1.py
@@ -1,17 +1,4 @@
 #!/usr/bin/env python3
-from tempfile import TemporaryFile
-# outfile = TemporaryFile()
-# np.savez(outfile, x=x, y=y)
-# _ = outfile.seek(0)
-# npzfile = np.load(outfile)
-# sorted(npzfile.files)
-# ['x', 'y']
-# npzfile['x']
-# array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-
-# np.save(outfile, x)
-# np.load(outfile)
-# array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 import matplotlib.pyplot as plt
 import matplotlib
